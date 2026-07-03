8cf584a33f69f90fff84830cdccd57b621008f9d
Woson. <16642832+Woson199@user.noreply.gitee.com>
2026-07-03 23:41:19 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿using System;
+int num;
+num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num);
---
+++ Enum/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace EnumApplication
+{
+    class Color
+    {
+        public enum ColorEnum
+        {
+            Red,
+            Green,
+            Blue
+        };
+        static void Main(string[] args)
+        {
+            ColorEnum color = ColorEnum.Red;
+            // 访问枚举对象
+            Console.WriteLine(color); 
+           
+            // 访问枚举对象的值
+            Console.WriteLine((int)color);
+
+            // 访问枚举对象的名称
+            Console.WriteLine(color.ToString());
+
+            // 定义对象的值
+            int val = 2;
+            // 根据值访问枚举对象
+            ColorEnum color2 = (ColorEnum)val;
+            Console.WriteLine(color2);
+
+            // 定义枚举对象的名称
+            string name = "Green";
+            // 根据名称访问枚举对象
+            ColorEnum color3 = Enum.Parse<ColorEnum>(name);
+            Console.WriteLine(color3); 
+
+            // 定义枚举对象的名称
+            string name2 = "Blue";
+            // 根据名称访问枚举对象
+            ColorEnum color4 = Enum.Parse<ColorEnum>(name2);
+            Console.WriteLine(color4);
+
+            string[] names = Enum.GetNames(typeof(ColorEnum));
+            foreach (string s in names)
+                Console.WriteLine(s);
+        }
+    }
+}
